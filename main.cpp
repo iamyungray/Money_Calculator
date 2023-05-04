d1503f30daa4812422f247daa6060af050d0ea43
iamyungray <49537486+iamyungray@users.noreply.github.com>
2023-05-04 14:11:52 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+    double cash;
+    double bank;
+    double total;
+
+    cout << "\nT";
+    Sleep(100);
+    cout << "o";
+    Sleep(100);
+    cout << "o";
+    Sleep(100);
+    cout << "l";
+    Sleep(100);
+    cout << " m";
+    Sleep(100);
+    cout << "a";
+    Sleep(100);
+    cout << "d";
+    Sleep(100);
+    cout << "e";
+    Sleep(100);
+    cout << " b";
+    Sleep(100);
+    cout << "y";
+    Sleep(100);
+    cout << " Y";
+    Sleep(100);
+    cout << "u";
+    Sleep(100);
+    cout << "n";
+    Sleep(100);
+    cout << "g";
+    Sleep(100);
+    cout << " R";
+    Sleep(100);
+    cout << "a";
+    Sleep(100);
+    cout << "y";
+    Sleep(100);
+    cout << ".";
+    Sleep(1000);
+    cout << ".";
+    Sleep(1000);
+    cout << ".";
+    Sleep(1000);
+
+    cout << "\n\n+++++++++++++++++++++\n\n";
+
+    cout << "     D";
+    Sleep(50);
+    cout << "I";
+    Sleep(50);
+    cout << "S";
+    Sleep(50);
+    cout << "C";
+    Sleep(50);
+    cout << "L";
+    Sleep(50);
+    cout << "A";
+    Sleep(50);
+    cout << "I";
+    Sleep(50);
+    cout << "M";
+    Sleep(50);
+    cout << "E";
+    Sleep(50);
+    cout << "R";
+    Sleep(250);
+    cout << "!";
+    Sleep(250);
+    cout << "\n\n+++++++++++++++++++++\n";
+    Sleep(1000);
+
+    cout << "\nO";
+    Sleep(100);
+    cout << "n";
+    Sleep(100);
+    cout << "l";
+    Sleep(100);
+    cout << "y";
+    Sleep(100);
+    cout << " u";
+    Sleep(100);
+    cout << "s";
+    Sleep(100);
+    cout << "e";
+    Sleep(100);
+    cout << " '.'";
+    Sleep(100);
+    cout << " i";
+    Sleep(100);
+    cout << "n";
+    Sleep(100);
+    cout << "s";
+    Sleep(100);
+    cout << "t";
+    Sleep(100);
+    cout << "e";
+    Sleep(100);
+    cout << "a";
+    Sleep(100);
+    cout << "d";
+    Sleep(100);
+    cout << " o";
+    Sleep(100);
+    cout << "f";
+    Sleep(100);
+    cout << " ','";
+    Sleep(100);
+    cout << " i";
+    Sleep(100);
+    cout << "f";
+    Sleep(100);
+    cout << " n";
+    Sleep(100);
+    cout << "e";
+    Sleep(100);
+    cout << "e";
+    Sleep(100);
+    cout << "d";
+    Sleep(100);
+    cout << "e";
+    Sleep(100);
+    cout << "d";
+    Sleep(100);
+    cout << ".\n\n";
+    Sleep(3000);
+
+    cout << "Cash total:\n>> $";
+    cin >> cash;
+    cout << "Bank total:\n>> $";
+    cin >> bank;
+    
+    total = cash + bank;
+
+    cout << "Money in total you broke ass: $" << total << "\n\n";
+
+    system("pause");
+
+    return 0;
+
+}
